c73bd49e8709a0e2767a28a90061e84f840938b8
Volgant <mr.volgant@mail.ru>
2021-02-21 23:01:29 +0300

    Change logic for test

--- test_items.py
+++ test_items.py
@@ -4,6 +4,9 @@ import time
 def test_open_browser_with_different_language(browser, user_language):
     link = "http://selenium1py.pythonanywhere.com/{0}/catalogue/coders-at-work_207/".format(user_language)
     browser.get(link)
-    browser.find_element_by_css_selector("form >button.btn-add-to-basket")
+    browser.implicitly_wait(7)
+    btn = browser.find_element_by_css_selector("form >button.btn-add-to-basket")
+    if btn:
+        print(btn.text)
     time.sleep(20)
 
