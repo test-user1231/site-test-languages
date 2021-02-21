bb60ae56b12b415dfcd1c3c3d1128263e079d6d9
Volgant <mr.volgant@mail.ru>
2021-02-21 23:14:13 +0300

    add assert instead if

--- test_items.py
+++ test_items.py
@@ -6,7 +6,6 @@ def test_open_browser_with_different_language(browser, user_language):
     browser.get(link)
     browser.implicitly_wait(7)
     btn = browser.find_element_by_css_selector("form >button.btn-add-to-basket")
-    if btn:
-        print(btn.text)
+    assert btn.is_displayed(), "Кнопка отсутствует"
     time.sleep(20)
 
